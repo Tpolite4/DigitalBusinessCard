65553bf5bac32bea7703815a681ea7b7dba85ad5
Tyler <tylerpolite@MacBook-Pro-2.local>
2025-09-02 20:21:10 -0400

    Profile Photo Populated

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ProfileCard from './components/ProfileCard';
+// import Content from './components/Content';
+// import Buttons from './components/Buttons';
+
+function App() {
+  return (
+    <div className='page'>
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/ProfileCard.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Profile from '../assets/profilepic.JPG';
+
+export default function ProfileCard() {
+  return <img src={Profile} alt='profile' />;
+}
